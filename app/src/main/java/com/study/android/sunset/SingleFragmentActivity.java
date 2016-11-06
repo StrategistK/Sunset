58e9a5749645a248e70fee4f27254503ef03f47a
strategist k <815034557@qq.com>
2016-11-06 14:47:30 +0800

    practice
    简单的动画

---
+++ app/src/main/java/com/study/android/sunset/SingleFragmentActivity.java
@@ -0,0 +1,34 @@
+package com.study.android.sunset;
+
+import android.os.Bundle;
+import android.support.annotation.LayoutRes;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/10/18.
+ */
+
+public abstract class SingleFragmentActivity extends AppCompatActivity {
+
+    protected abstract android.support.v4.app.Fragment createFragment();
+
+    @LayoutRes
+    protected int getLayoutResId() {
+        return R.layout.activity_fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getLayoutResId());
+
+        FragmentManager fm = getSupportFragmentManager();
+        android.support.v4.app.Fragment fragment = fm.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null) {
+            fragment = createFragment();
+            fm.beginTransaction().add(R.id.fragment_container,fragment).commit();
+        }
+    }
+}
